5f941e5ecbdf4a66bec28dbca026216fb6f96093
Fez04 <david_avendano@hotmail.com>
2018-01-17 18:59:53 -0800

    console log and main

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+console.log("I want to grow, share and challange myself in this class!");
+
+main = document.querySelector('#main');
+
+main.innerHTML = 'I want to grow, share and challange myself in this class!'
