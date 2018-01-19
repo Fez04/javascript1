37af7c73dfd4badfeacad146c2e161944fce32ab
Fez04 <david_avendano@hotmail.com>
2018-01-18 21:00:18 -0800

    var obj und

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+var sentence = 2;
+console.log(sentence);
+{
+const truth = "Ep8 was ....TERRIBLE...:";
+console.log(truth);
+}
+// console.log(truth);
+
+
+const truth = "Ep8 was ....TERRIBLE...:";
+console.log(truth);
+// console.log(truth);
+
+
+let rightNow = new Date();
+console.log(rightNow);
+//Thu Jan 18 2018 20:43:07 GMT-0800 (Pacific Standard Time);
+
+var string = "lalalalal";
+
+var newString = new String("lalalalal");
+
+console.log(string.toUpperCase());
+console.log(newString.toUppercase());
