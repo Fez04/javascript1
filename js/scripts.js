a54ce5a79b78366acafdda39754a06d5487f1b17
Fez04 <david_avendano@hotmail.com>
2018-01-23 19:28:35 -0800

    this is my gh-pages branch

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
-/*var firstname = prompt("Fist Name");
+var firstname = prompt("Fist Name");
 console.log(firstname);
 var lastname = prompt("Last Name");
 alert(lastname);
 let birthday = new Date();
-result = window.confirm(birthday); */
+result = window.confirm(birthday);
 
-/*let birthday = prompt("Whats Your Birthday?", "Please answer in the following format: 1/1/1900");
-confirm(new Date(birthday));*/
+let birthday = prompt("Whats Your Birthday?", "Please answer in the following format: 1/1/1900");
+confirm(new Date(birthday));
 
 
 
@@ -23,7 +23,7 @@ confirm(new Date(birthday));*/
 //let HellowMessage =( "hey, there! My book is only $9.99....");
 
 
-/*let confirmMessage = confirm("is your name david?");
+/*let configitrmMessage = confirm("is your name david?");
 console.log(confirmmessage);*/
 
 /*let promptMessage = prompt("Tell me the password...", "password");
